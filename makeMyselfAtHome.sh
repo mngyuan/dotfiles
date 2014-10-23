a215786d02c358193cac01ee90b9df7555dfbf97
Kevin Lee <kevinmlee.x@gmail.com>
2014-10-23 15:50:47 -0700

    update server script

--- makeMyselfAtHome.sh
+++ makeMyselfAtHome.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-apt-get install git
+apt-get install --assume-yes git
 mkdir ~/git
 mkdir ~/git/agorascript
-git clone git@github.com:agoraphobiae/agorascript.git ~/git/agorascript
+git clone https://github.com/agoraphobiae/agorascript.git ~/git/agorascript
 ln -s ~/git/agorascript/agoraphobiae_rc ~/agoraphobiae_rc
 echo -e "\n# KL\nsource ~/agoraphobiae_rc" >> ~/.bash_profile
+ssh-keygen
+cd ~/git/agorascript && git remote set-url origin git@github.com:agoraphobiae/agorascript.git
