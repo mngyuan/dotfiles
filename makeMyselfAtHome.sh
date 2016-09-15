99171fc9a706b57841bb69f9d44e4c41b8da543b
Kevin Lee <phorust@fb.com>
2016-09-14 17:25:15 -0700

    add autosuggestions to makemyselfathome

--- makeMyselfAtHome.sh
+++ makeMyselfAtHome.sh
@@ -49,6 +49,7 @@ fi
 # set up oh-my-zsh and extra zsh stuff
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 # set up extra vim stuff
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
