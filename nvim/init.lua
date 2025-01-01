bdbc7c82ee073e04479251afe9d8449317ae199b
Kevin Lee <hikevinmlee@gmail.com>
2025-01-01 22:23:23 +0800

    use vim clipboard, ; as :

--- nvim/init.lua
+++ nvim/init.lua
@@ -114,9 +114,9 @@ vim.opt.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
-end)
+-- vim.schedule(function()
+-- 	vim.opt.clipboard = "unnamedplus"
+-- end)
 
 -- Enable break indent
 vim.opt.breakindent = true
@@ -159,6 +159,9 @@ vim.opt.scrolloff = 10
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+--  mngyuan
+vim.keymap.set("n", ";", ":")
+--  endmngyuan
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
