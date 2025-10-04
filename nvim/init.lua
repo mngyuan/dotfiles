726813f9358a1fd018c50dcf2711b8d4abb507b1
Kevin Lee <hikevinmlee@gmail.com>
2025-10-04 16:10:54 +0100

    add color code highlighter to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1032,6 +1032,12 @@ require("lazy").setup({
 		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
 		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	},
+	{ -- Specifically for highlighting color codes
+		"catgoose/nvim-colorizer.lua",
+		event = "BufReadPre",
+		opts = { -- set to setup table
+		},
+	},
 
 	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
